5a8e21b9cf5ef01e694abd6ce1fc07164501c20b
voirtimid <=>
2020-06-10 23:14:32 +0200

    Removed headers

--- frontend/src/custom-axios/axios.ts
+++ frontend/src/custom-axios/axios.ts
@@ -1,11 +1,7 @@
 import axios from 'axios'
 
 const axios_instance = axios.create({
-    baseURL: 'http://gateway:8080',
-    headers: {
-        "Origin": "http://frontend",
-        "Access-Control-Allow-Origin": "*"
-    }
+    baseURL: 'http://gateway:8080'
 });
 
 export default axios_instance;
